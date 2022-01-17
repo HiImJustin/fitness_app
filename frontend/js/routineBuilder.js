50683c4f94b36c0ed539e99bf2a28e5963830418
HiImJustin <jcoates1997@hotmail.com>
2022-01-17 11:54:04 +1000

    commit

--- frontend/js/routineBuilder.js
+++ frontend/js/routineBuilder.js
@@ -17,15 +17,8 @@ fetch("/api/exercises")
 
 function selectExercises() {
     
+    //Get 
     let exercise_options = document.querySelector("#exercise-options")
-    exercise_options.addEventListener("click", doSomething, false)
-
-
-    //Click on each avaliable options and turn the backgroundcolor to green to show its selected, need to be able to do it for all and click it to unselect back to grey
-}
-
-
-let exercise_options = document.querySelector("#exercise-options")
     exercise_options.addEventListener("click", doSomething, false)
 
     function doSomething(e) {
@@ -35,3 +28,9 @@ let exercise_options = document.querySelector("#exercise-options")
         }
         e.stopPropagation();
     }
+
+
+    //Click on each avaliable options and turn the backgroundcolor to green to show its selected, need to be able to do it for all and click it to unselect back to grey
+}
+
+
